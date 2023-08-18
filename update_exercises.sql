2266181c39286549e6d32d8c93049b546f31ca57
Jesse <Jdorman1289@gmail.com>
2023-08-18 12:12:05 -0500

    create update_exercises script data

---
+++ update_exercises.sql
@@ -0,0 +1,22 @@
+-- All albums in your table
+SELECT * FROM albums;
+-- No UPDATE statement needed for this query
+
+-- All albums released before 1980
+SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums SET sales = sales * 10;
+-- No need to update the release_date for this query
+
+-- All albums by Michael Jackson
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET sales = sales * 10;
+-- No need to update the release_date for this query
+
+-- Move all albums before 1980 back to the 1800s
+SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums SET release_date = release_date - 100;
+-- No need to update the sales for this query
+
+-- Change 'Michael Jackson' to 'Peter Jackson'
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson';
