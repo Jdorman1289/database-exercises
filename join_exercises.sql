89c6b88d3a5f1c3f85f36ee5fdfc8e1a3e95d0d2
Jesse <Jdorman1289@gmail.com>
2023-08-22 15:50:32 -0500

    finish join_exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,3 +1,5 @@
+USE employees;
+
 SELECT d.dept_name AS D_Name, CONCAT(e.first_name, ' ', e.last_name) AS D_Manager
 FROM departments d
          JOIN dept_manager dm
@@ -7,4 +9,25 @@ FROM departments d
 WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
-USE employees;
+
+SELECT d.dept_name AS D_Name, CONCAT(e.first_name, ' ', e.last_name) AS D_Manager
+FROM departments d
+         JOIN dept_manager dm
+              ON d.dept_no = dm.dept_no
+         JOIN employees e
+              ON dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01'
+  AND e.gender = 'F'
+ORDER BY d.dept_name;
+
+SELECT DISTINCT t.title AS title, COUNT(*) AS Total
+FROM departments d
+         JOIN dept_manager dm
+              ON d.dept_no = dm.dept_no
+         JOIN  titles t
+              ON dm.emp_no = t.emp_no
+WHERE dm.to_date = '9999-01-01'
+GROUP BY t.title;
+
+
+
